43e25e7559a472ef03d154f26b60a4d04c094f90
Dmitry Danilov <labamnus@yandex.ru>
2023-03-06 11:16:20 +0600

    Update data models commit

--- migrations/00001_init.up.sql
+++ migrations/00001_init.up.sql
@@ -17,63 +17,83 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE public.user
 (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(255),
-    email VARCHAR(255) UNIQUE ,
-    CONSTRAINT proper_email CHECK (email ~* '^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'),
-    guest_id TEXT UNIQUE,
+    id       UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name     VARCHAR(255),
+    phone    VARCHAR(255) UNIQUE,
     password TEXT,
-    image_id TEXT,
-    is_admin BOOLEAN DEFAULT false
+    profileImage TEXT,
+    isAdmin BOOLEAN          DEFAULT false
 );
 
-CREATE TABLE public.author
+CREATE TABLE public.session
 (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(255) NOT NULL,
-    image_id TEXT
-);
-
-CREATE TABLE public.story
-(
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
-    image_id UUID,
-    text TEXT NOT NULL,
-    read_time SMALLINT,
-    rating FLOAT
+    callId TEXT NOT NULL,
+    code TEXT NOT NULL,
+    isConfirmed BOOLEAN DEFAULT false,
+    deviceName TEXT,
+    hashRt TEXT,
+    createdAt TIMESTAMP DEFAULT now()
 );
 
 CREATE TABLE public.quote
 (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    text TEXT NOT NULL,
-    story_id UUID NOT NULL,
-    CONSTRAINT story_fk FOREIGN KEY (story_id) REFERENCES public.story(id),
-    user_id UUID NOT NULL,
-    CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES public.user(id)
+    id       UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    content  TEXT NOT NULL,
+    storyId UUID NOT NULL,
+    CONSTRAINT story_fk FOREIGN KEY (storyId) REFERENCES public.story (id),
+    userId  UUID NOT NULL,
+    CONSTRAINT user_fk FOREIGN KEY (userId) REFERENCES public.user (id),
+    tagId   UUID,
+    constraint quote_tag_fk FOREIGN KEY (tagId) REFERENCES public.quote_tags (id)
 );
 
-CREATE TABLE public.stories_authors (
-    story_id UUID NOT NULL,
-    CONSTRAINT story_fk FOREIGN KEY (story_id) REFERENCES public.story(id),
-    author_id UUID NOT NULL,
-    CONSTRAINT author_fk FOREIGN KEY (author_id) REFERENCES public.author(id)
+CREATE TABLE public.quote_tags
+(
+    id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    userId     UUID NOT NULL,
+    CONSTRAINT user_fk FOREIGN KEY (userId) REFERENCES public.user (id),
+    description TEXT,
+    color       TEXT NOT NULL
+);
+
+CREATE TABLE public.story
+(
+    id       UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    title    VARCHAR(255) NOT NULL,
+    cover    TEXT,
+    content  TEXT         NOT NULL,
+    duration SMALLINT,
+    rating   FLOAT
+);
+
+CREATE TABLE public.author
+(
+    id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name        VARCHAR(255) NOT NULL,
+    description TEXT
+);
+
+CREATE TABLE public.stories_authors
+(
+    storyId  UUID NOT NULL,
+    CONSTRAINT story_fk FOREIGN KEY (storyId) REFERENCES public.story (id),
+    authorId UUID NOT NULL,
+    CONSTRAINT author_fk FOREIGN KEY (authorId) REFERENCES public.author (id)
 );
 
 CREATE TABLE public.tag
 (
-    id SERIAL PRIMARY KEY,
+    id   SERIAL PRIMARY KEY,
     name TEXT NOT NULL
 );
 
 CREATE TABLE public.stories_tags
 (
-    story_id UUID NOT NULL,
-    CONSTRAINT story_fk FOREIGN KEY (story_id) REFERENCES public.story(id),
-    tag_id UUID NOT NULL,
-    CONSTRAINT tag_fk FOREIGN KEY (tag_id) REFERENCES public.tag(id)
+    storyId UUID NOT NULL,
+    CONSTRAINT story_fk FOREIGN KEY (storyId) REFERENCES public.story (id),
+    tagId   UUID NOT NULL,
+    CONSTRAINT tag_fk FOREIGN KEY (tagId) REFERENCES public.tag (id)
 );
 
 COMMIT;
